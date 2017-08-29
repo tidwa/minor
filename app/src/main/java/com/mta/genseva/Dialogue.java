17c6450eb95efac91da63a9e09a94c1512510781
swap-mta <96swapnilsharma@gmail.com>
2017-08-29 13:24:59 -0700

    second commit

---
+++ app/src/main/java/com/mta/genseva/Dialogue.java
@@ -0,0 +1,13 @@
+package com.mta.genseva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dialogue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogue);
+    }
+}
